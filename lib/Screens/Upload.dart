00f18e553c91d00b26ea05874665c2bb5e9491d9
Hafsa19197 <hafsaayyhere@gmail.com>
2023-08-17 06:16:04 -0700

    eph

--- lib/Screens/Upload.dart
+++ lib/Screens/Upload.dart
@@ -4,13 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'dart:io';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import '../_Globals/_GlobalMethods.dart';
 import '../_Globals/imagesApi.dart';
-import 'dart:convert';
-import 'dart:developer' as developer;
+
 
 class UploadQuiz extends StatefulWidget {
   const UploadQuiz({super.key});
